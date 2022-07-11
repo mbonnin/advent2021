c216c2245c739e4a0e1fd448f68604e6470e0cfa
Mark Bonnin <mbonnin@gmail.com>
2022-07-10 21:38:55 -0400

    add day 2 part 1

---
+++ advent2.1/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	totalDistance, totalDepth := 0, 0
+
+	fetchInput("input2.txt", totalDistance, totalDepth)
+
+}
+
+func fetchInput(path string, x int, y int) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		parsedLine := strings.Fields(scanner.Text())
+		direction := parsedLine[0]
+		amount, err := strconv.Atoi(parsedLine[1])
+		if err != nil {
+			panic(err)
+		}
+
+		x, y = updateLocation(direction, amount, x, y)
+	}
+
+	fmt.Printf("x: %d y: %d\n", x, y)
+	fmt.Printf("total: %d\n", x*y)
+}
+
+func updateLocation(direction string, amount int, x int, y int) (int, int) {
+	if direction == "forward" {
+		x += amount
+	}
+	if direction == "up" {
+		y -= amount
+	}
+	if direction == "down" {
+		y += amount
+	}
+
+	return x, y
+}
